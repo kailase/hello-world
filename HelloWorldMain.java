57cabaad485012262365ff8e5c0b54cb7e525d56
kailase <52557917+kailase@users.noreply.github.com>
2019-07-15 12:08:01 +0530

    Add files via upload

---
+++ HelloWorldMain.java
@@ -0,0 +1,13 @@
+package com.fss;
+
+public class HelloWorldMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a test project.");
+		
+		System.out.println("This is a change that is introduced.");
+
+	}
+
+}
